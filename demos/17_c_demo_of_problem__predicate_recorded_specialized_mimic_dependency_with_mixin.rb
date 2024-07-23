5a07746654dac9631d858ae6583d22cf76a0e4b8
Scott Bellware <sbellware@gmail.com>
2024-07-23 16:10:46 -0500

    Demo of problem where RecordInvocation is implicit

---
+++ demos/17_c_demo_of_problem__predicate_recorded_specialized_mimic_dependency_with_mixin.rb
@@ -0,0 +1,45 @@
+require_relative 'demo_init'
+
+
+class HTTPClient
+  def post(content)
+    # ...
+  end
+
+  module Substitute
+    ## This code can be deactivated without causing an error
+    # include RecordInvocation
+
+    # record def post(content)
+    #   puts "Content: #{content}"
+    # end
+
+    ## Mimic includes RecordInvocation in all substitutes by default
+    ## which makes `invoked?` seem to come out of nowhere
+
+    # def posted?(content)
+    #   invoked?(:post, content: content)
+    # end
+  end
+end
+
+class Upload
+  include Dependency
+
+  dependency :http_client, HTTPClient
+
+  def call(file)
+    http_client.post(file)
+  end
+end
+
+upload = Upload.new
+
+content = "some file"
+
+upload.(content)
+
+# upload.http_client.posted?("some file")
+# => true
+
+upload.http_client.invoked?(:post, content: content)
